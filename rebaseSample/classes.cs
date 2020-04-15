ac633b7d4a26e93a0a9e424620024be1a1f7f36f
Mike Sarkezians <Mike.Sarkezians@gmail.com>
2020-04-14 20:32:31 -0400

    m1

---
+++ rebaseSample/classes.cs
@@ -0,0 +1,31 @@
+public enum SalesTax
+    {
+        ThirteenPercent = 0,
+        FiftheenPercent = 1,
+        SixteePercent = 2
+    }
+
+    public enum SalesTaxFromFeature
+    {
+        ThirteenPercent = 0,
+        FiftheenPercent = 1,
+        SixteePercent = 2
+    }
+
+    public struct NameOnly
+    {
+        public string structName { get; set; }
+    }
+
+    public struct RebaseTest{
+        public string DataName;
+    }
+
+    public struct NameOnlyEx
+    {
+        public string structName { get; set; }
+    }
+
+    public struct RebaseTestEx{
+        public string DataName;
+    }
