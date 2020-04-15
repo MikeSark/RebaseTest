5f7dd29de7272a9bcba28cb6aecec63a82e472e2
Mike Sarkezians <Mike.Sarkezians@gmail.com>
2020-04-14 20:20:12 -0400

    Add project files.

---
+++ rebaseSample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace rebaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int Getlimit(string name)
+        {
+            return name.Length;
+        }
+
+        public int GetlimitEx(string name)
+        {
+            return name.Length * 2;
+        }
+
+    }
+
+    public enum SalesTax
+    {
+        ThirteenPercent = 0,
+        FiftheenPercent = 1,
+        SixteePercent = 2
+    }
+
+    public enum SalesTaxFromFeature
+    {
+        ThirteenPercent = 0,
+        FiftheenPercent = 1,
+        SixteePercent = 2
+    }
+
+    public struct NameOnly
+    {
+        public string structName { get; set; }
+    }
+
+    public struct RebaseTest{
+        public string DataName;
+    }
+
+    public struct NameOnlyEx
+    {
+        public string structName { get; set; }
+    }
+
+    public struct RebaseTestEx{
+        public string DataName;
+    }
+}
+
+
+
